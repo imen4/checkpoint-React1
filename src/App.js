2c582c079fbfac850ea50afb8e5c8c79aa2fe7c7
Imen <imenjlassi02@gmail.com>
2022-10-06 19:03:55 +0100

    add checkpoint React Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
-import Cards from "./Components-wp/CardContainer";
-
-
+import Profile from "./profile/Profile";
 function App() {
+
+  const handleName  = name => alert(name);
+  const styleObjet = {border:"black double",  margin:"20px", width:"85vh" ,padding:"20px", position: "absolute",
+  left: "30%"}
   return (
-    <div className="parent">
-     <Cards/>
+    <div style={styleObjet}>
+     <Profile fullName="🎀  𝒜𝓀𝒾 𝒦ō𝒹ā  🎀"  bio="defy difficulties😣, put effort, fall and get up, keep learning and hope to be a developer😊!! 🎵 I'm on my way i can go the distance~ i don't care how far somehow i be strong 💪!🎵" 
+     profession="developer 💻" alertMyInput={handleName}> 
+     /asset/anime.jpg </Profile>
     </div>
   );
 }
 
 export default App;
+
+
+
+
