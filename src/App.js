a1f7db8b8dfe7414b76486f6dfd3e04c261f82e8
Imen <imenjlassi02@gmail.com>
2022-10-09 15:50:36 +0100

    wp_React_Props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import { PropTypes } from "prop-types";
 import "./App.css";
-
-const Name =(props)=>{
-  console.log(props)
-  return( <h1>Bonour, {props.name}</h1>);
-}
-Name.defaultProps={
-  name:"firas"
-}
+import ListCars from "./wp-props/ListCars";
+import mercedes from "./assets/mercedes.jpg";
+import ferrari from "./assets/ferrari.jpg";
+import porche from "./assets/porche.jpg"
 
 function App(props) {
-  const name ="imen"
+  const carList=[
+    {
+      id:1,
+      img:mercedes,
+      name:"Mercedes",
+      price:"300 000 $"
+    },
+    {
+      id:2,
+      img:ferrari,
+      name:"Ferrari",
+      price:"420 000 $"
+    },
+    {
+      id:3,
+      img:porche,
+      name:"Porche",
+      price:"200 000 $"
+    }
+  ]
+  const testCallBack = name =>alert(`hello ${name}`)
   return (
     <div >
-    <Name name= {name}/>
+      <ListCars carList={carList} testCallBack={testCallBack}/>
     </div>
   );
   
 }
 
-Name.propTypes ={
-  name:PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-}
+
 
 export default App;
 
