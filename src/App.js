d108099e5935f1630068e85cd1735d29a4e87cb2
Imen <imenjlassi02@gmail.com>
2022-10-10 19:40:52 +0100

    add checkpoint react_State

--- src/App.js
+++ src/App.js
@@ -1,43 +1,64 @@
 import "./App.css";
-import ListCars from "./wp-props/ListCars";
-import mercedes from "./assets/mercedes.jpg";
-import ferrari from "./assets/ferrari.jpg";
-import porche from "./assets/porche.jpg"
+import React from "react";
+import anime from "./assets/anime.jpg";
+import Button from "react-bootstrap/Button";
 
-function App(props) {
-  const carList=[
-    {
-      id:1,
-      img:mercedes,
-      name:"Mercedes",
-      price:"300 000 $"
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "Aki Kōdā",
+      bio: "defy difficulties😣, put effort, fall and get up, keep learning and hope to be a developer😊!!",
+      imgSrc: anime,
+      profession: "developer 💻",
     },
-    {
-      id:2,
-      img:ferrari,
-      name:"Ferrari",
-      price:"420 000 $"
-    },
-    {
-      id:3,
-      img:porche,
-      name:"Porche",
-      price:"200 000 $"
-    }
-  ]
-  const testCallBack = name =>alert(`hello ${name}`)
-  return (
-    <div >
-      <ListCars carList={carList} testCallBack={testCallBack}/>
-    </div>
-  );
-  
+    show: false,
+    count: 0
+  }
+  handlerClick = () => {
+    this.setState((currentState) => ({ show: !currentState.show }))
+  }
+  componentDidMount() {
+    this.intervalId  = setInterval(
+      () => {
+        this.setState(prevState => {
+          return {
+            count: prevState.count + 1,
+          };
+        });
+      }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId );
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Button onClick={this.handlerClick} variant="primary">
+          {" "}
+          {this.state.show ? "hide" : "show"}
+        </Button>
+        {this.state.show && (
+          <div style={{ marginTop: "10px" }}>
+            <span>Name: {this.state.Person.fullName}</span>
+            <div>
+              <span>Bio: {this.state.Person.bio}</span>
+            </div>
+            <span>Profession: {this.state.Person.profession}</span>
+            <img src={anime} alt="anime" width={200} />
+          </div>
+        )}
+          <h2>The component has been rendered for {this.state.count} seconds</h2>
+      </div>
+    );
+  }
 }
 
-
-
 export default App;
-
-
-
-
